a3a87df970ea4dafedaf9cc8c99d3cda3082f7cc
Shunsuke <shun.sskd05@icloud.com>
2019-11-28 08:51:24 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	def hello
+		render html: "hello,world¡"
+	end
+
+	def goodbye
+		render html: "goodbye,world!"
+	end
+end
